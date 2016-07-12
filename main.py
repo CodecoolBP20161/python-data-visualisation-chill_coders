8272371b45ba5ab053112a27c323f59b12a4eded
dsztanko <szt.dori@gmail.com>
2016-07-12 12:58:46 +0200

    clarifies callings

--- main.py
+++ main.py
@@ -38,24 +38,58 @@ Choose a number from 1 to 5 create a WordCloud!
 5: Categories of status according to number of projects
 """)
 
-number = input('Choosen number is: ')
-
 while True:
-        if number == '1':
-            print(Operator.operator_main("""SELECT company_name, replace(main_color, '#', '') as short_hex_color FROM project WHERE main_color IS NOT NULL;""", "q1"))
-            exit()
-        if number == '2':
-            print(Normalisation.normalize_data_q2((Operator.operator_main("""SELECT name, budget_value, budget_currency, REPLACE(main_color, '#', '') AS main_color FROM project WHERE name IS NOT NULL AND main_color IS NOT NULL;""", "q4"))))
-            exit()
-        if number == '3':
-            print(Normalisation.normalize_data_q3(Operator.operator_main("""SELECT name, duedate, replace(main_color, '#', '') AS main_color FROM project WHERE company_name = 'Eadel' AND name IS NOT NULL ORDER BY duedate DESC;""", "")))
-            exit()
-        if number == '4':
-            print(Operator.operator_main("""SELECT company_hq, budget_value, replace(main_color, '#', '') AS main_color FROM project WHERE budget_currency = 'GBP' AND company_hq IS NOT NULL AND main_color IS NOT NULL ORDER BY CAST(budget_value AS FLOAT) DESC;""",""))
-            exit()
-        if number == '5':
-            print(Normalisation.normalize_data_q5(Operator.operator_main("""SELECT name, status, REPLACE(main_color, '#', '') AS main_color FROM project WHERE name IS NOT NULL AND main_color IS NOT NULL ORDER BY status DESC;""","")))
-            exit()
-        else:
-            raise ValueError('Invalid input!')
-            exit()
+    number = input('Choosen number is: ')
+
+    if number == '1':
+        q1 = """SELECT company_name, replace(main_color, '#', '') as short_hex_color
+                FROM project
+                WHERE main_color IS NOT NULL;"""
+        order = "q1"
+        print(Operator.operator_main(q1, order))
+        exit()
+
+    if number == '2':
+        q2 = """SELECT name, budget_value, budget_currency, REPLACE(main_color, '#', '') AS main_color
+                FROM project
+                WHERE name IS NOT NULL
+                AND main_color IS NOT NULL;"""
+        order = "q4"
+        normalised_incoming_datalist = Normalisation.normalize_data_q2(Operator.operator_main(q2, order))
+        print(Operator.getsize(normalised_incoming_datalist))
+        exit()
+
+    if number == '3':
+        q3 = """SELECT name, duedate, replace(main_color, '#', '') AS main_color
+                FROM project
+                WHERE company_name = 'Eadel'
+                AND name IS NOT NULL ORDER BY duedate DESC;"""
+        order = ""
+        normalised_incoming_datalist = Normalisation.normalize_data_q3(Operator.operator_main(q3, order))
+        print(Operator.getsize(normalised_incoming_datalist))
+        exit()
+
+    if number == '4':
+        q4 = """SELECT company_hq, CAST(budget_value AS float), replace(main_color, '#', '') AS main_color
+                FROM project
+                WHERE budget_currency = 'GBP'
+                AND company_hq IS NOT NULL
+                AND main_color IS NOT NULL ORDER BY CAST(budget_value AS FLOAT) DESC;"""
+        order = ""
+        normalised_incoming_datalist = Operator.operator_main(q4, order)
+        print(Operator.getsize(normalised_incoming_datalist))
+        exit()
+
+    if number == '5':
+        q5 = """SELECT name, status, REPLACE(main_color, '#', '') AS main_color
+                FROM project
+                WHERE name IS NOT NULL
+                AND main_color IS NOT NULL ORDER BY status DESC;"""
+        order = ""
+        normalised_incoming_datalist = Normalisation.normalize_data_q5(Operator.operator_main(q5, order))
+        print(Operator.getsize(normalised_incoming_datalist))
+        exit()
+
+    else:
+        print('Invalid input!')
+        exit()
